f981baeb04d9298f2c8fb312fd95bd2aa9a0fe43
Votre Nom <Vous@exemple.com>
2025-05-23 13:50:32 +0200

    MAster texts and actions

--- server/public/js/master.js
+++ server/public/js/master.js
@@ -11,34 +11,43 @@ submit.addEventListener('click', () => {
   submit.remove()
 })
 
-let actions = "qeokszaàmxlhpctrfbvi_".split("");
 
-for (let index = 0; index < actions.length; index++) {
-  const action = actions[index];
-  const btn = document.createElement('button');
-  btn.dataset.action = action;
-  btn.textContent = action;
-  document.querySelector('.actions').appendChild(btn)
-  btn.onclick = () => {
-    socket.emit("master action", btn.dataset.action);
-  }  
+async function getData() {
+  const url = "js/cards.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const cards = [];
+    const json = await response.json();
+    json.forEach((entry, idx) => {
+      cards.push({
+        shortcut: entry.shortcut,
+        text: entry.description,
+        type: entry.type
+      })         
+    });
+
+    return cards;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-const texte = [
-  "texte super long qui dépasse",
-  "text 2"
-];
-
-for (let index = 1; index <= texte.length; index++) {
-  const t = texte[index-1];
-  const btn = document.createElement('button');
-  btn.dataset.textid = index;
-  btn.textContent = t;
-  document.querySelector('.texts').appendChild(btn)
-  btn.onclick = () => {
-    socket.emit("master text", btn.dataset.textid);
-  }  
-}
-
-
+getData().then((cards) => {
+  cards.forEach(entry => {
+    const btn = document.createElement('button');
+    btn.dataset.textid = entry.shortcut;
+    btn.className = entry.type
+    btn.textContent = entry.text;
+    document.querySelector('.texts').appendChild(btn);
+    btn.onclick = () => {
+      console.log(entry.shortcut);
+      socket.emit("card change", btn.dataset.textid);
+    }
+  });
+    
+})
 
