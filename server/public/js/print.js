d43d5d900b47a0521460b42c83a8f94735d90210
Julien Bidoret <julien@accentgrave.net>
2025-06-18 16:50:18 +0200

    Print styles

--- server/public/js/print.js
+++ server/public/js/print.js
@@ -1,3 +1,7 @@
+// import paged.js module
+// import { log } from 'node:console';
+import { Previewer } from './paged.esm.js';
+
 const socket = io();
 
 socket.on('print', (msg) => {
@@ -6,11 +10,40 @@ socket.on('print', (msg) => {
     const tb = document.querySelector("#textbox");
     if(tb) tb.remove()
     document.body.insertAdjacentHTML("afterbegin", text_to_print);
+        
     const counterDisplay = document.createElement("div");
     counterDisplay.textContent = print_counter;
     counterDisplay.id = "print-counter";
-    document.body.appendChild(counterDisplay);
+    document.body.insertAdjacentElement("afterbegin", counterDisplay);
 
-    window.print()
+    doYourJob()
+    
+    
 });
   
+function doYourJob() {
+    
+    // gather all stylesheets within the document
+    const stylesheets = [...document.querySelectorAll('link[rel="stylesheet"]')].map(sheet => sheet.href);
+    
+    // gather all inline style tags
+    const styles = [...document.querySelectorAll('style')];
+    if (styles) {
+        const inline_styles = styles.map(s => s.textContent).join("");
+        // _nombidon is _really_ important
+        stylesheets.push({ _nombidon: inline_styles })  
+    }
+
+    // instanciate the Previewer
+    const previewer = new Previewer() 
+
+    // run
+    previewer.preview('', stylesheets);
+
+    setTimeout(() => {
+        // window.print()
+        console.log("yop");
+        
+    }, 500);
+
+}
